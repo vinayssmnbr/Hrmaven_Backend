5c482d52c988ec4347cc2c9dd23d607130726b43
Atul.Gupta4 <atul.gupta@venturepact.com>
2023-04-01 10:55:49 +0530

    login/helper/token

--- app/middlewares/authentication.js
+++ app/middlewares/authentication.js
@@ -1,4 +1,34 @@
-exports.verify = async function (req,res,next)
+const jwt=require('jsonwebtoken')
+
+const User = require('../models/credential');
+
+const verify = async function(req,res,next)
 {
-  next();
+  console.log('In Verify Token') 
+  const token = req.headers.authorization.split(' ')[1];
+  if (!token) return res.status(401).send('Access Denied !');
+
+
+  console.log("abc",token);
+
+  try 
+  {
+      const UserID= await jwt.verify(token, process.env.JWT_TOKEN_KEY);
+      console.log("ue",UserID)
+      if(!UserID){
+          return res.send("tst")
+
+      }
+      req.user = UserID;  
+      next();
+  } 
+  catch (error) 
+  
+  {
+    console.log(error)
+      res.status(400).send('Invalid token !');
+  }
+ 
 }
+
+module.exports = verify
