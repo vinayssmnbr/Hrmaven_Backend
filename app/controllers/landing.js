447eda97ba524e0009957b72810436ebedd883e3
prince sharma <prince.sharma@venturepact.com>
2023-04-01 10:43:17 +0530

    include all

--- app/controllers/landing.js
+++ app/controllers/landing.js
@@ -3,18 +3,10 @@ const User = require("../models/credential");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const userService = require('../services/user')
-const help = require('../helper/helper');
 
-exports.login = async function (req, res) {
- help.login(req, res);
+
+
+
+exports.signup = async function(req, res) {
+    userService.addUser(req, res);
 };
-
-exports.signup = async function (req, res) {
-  userService.addUser(req, res);
-};
-
-exports.auth = async function (req, res) {
-   res.send({
-    message: "dashboard"
-   });
-}
