cd820d806a531d6cd94459e4aea35c31933e1b65
vinayssmnbr <vinayssmnbr@gmail.com>
2023-04-16 01:13:19 +0530

    employee model update

--- app/models/employee/employeeModel.js
+++ app/models/employee/employeeModel.js
@@ -15,113 +15,121 @@ const employeeSchema = new Schema({
     type: Date,
     default: Date.now,
   },
-  city:{
-    type:String
-},
-postalCode:{
-    type:Number
-},
-state:{
-    type:String
-},
-mobile:{
-    type:Number
-},
-address:{
-    type:String
-},
-email:{
-    type:String
-},
-fatherName:{
-  type:String
-},
-motherName:{
-  type:String
-},
-maritalStatus:{
-  type:String,
-  enum:['Married','Unmarried']
-},
-bloodGroup:{
-  type:String,
-  enum:['A+','A-','B+','B-','O+','O-','AB+','AB-']
-},
-nationality:{
-  type:String
-},
-dateOfBirth:{
-  type:Date
-},
-gender:{
-  type:String,
-  enum:['Male','Female','Other']
-},
-bankname:{
-  type:String
-},
-accountno:{
-  type:Number
-},
-ifsc:{
-  type:String
-},
-adhaarno:{
-  type:Number
-},
-panno:{
-  type:String
-},
-passport:{
-  type:Number
-},
-matric:{
-  type:String
-
-},
-matricPercent:{
-  type:Number
-},
-inter:{
-  type:String
-},
-interPercent:{
-  type:Number
-},
-graduation:{
-  type:String
-},
-graduationStream:{
-  Type:String
-},
-graduationCgpa:{
-  type:Number
-},
-pg:{
-  type:String
-},
-pgStream:{
-  type:String
-},
-pgCgpa:{
-  type:String  
-},
-company:{
-  type:String
-},
-duration:{
-  from:Date,
-  to:Date
-},
-designation:{
-  type:String,
-  enum:[ 'Software Developer', 'Forntend Developer', 'Full Stack Developer','UI/UX Designer']
-},
-
+  city: {
+    type: String,
+  },
+  postalCode: {
+    type: Number,
+  },
+  state: {
+    type: String,
+  },
+  mobile: {
+    type: Number,
+  },
+  address: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  fatherName: {
+    type: String,
+  },
+  motherName: {
+    type: String,
+  },
+  maritalStatus: {
+    type: String,
+    enum: ["Married", "Unmarried"],
+  },
+  bloodGroup: {
+    type: String,
+    enum: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"],
+  },
+  nationality: {
+    type: String,
+  },
+  dateOfBirth: {
+    type: Date,
+  },
+  gender: {
+    type: String,
+    enum: ["Male", "Female", "Other"],
+  },
+  bankname: {
+    type: String,
+    enum: [
+      "State Bank Of India",
+      "Punjab National Bank",
+      "Central Bank Of India",
+      "HDFC Bank",
+      "ICICI Bank",
+    ],
+  },
+  accountno: {
+    type: Number,
+  },
+  ifsc: {
+    type: String,
+  },
+  adhaarno: {
+    type: Number,
+  },
+  panno: {
+    type: String,
+  },
+  passport: {
+    type: Number,
+  },
+  matric: {
+    type: String,
+  },
+  matricPercent: {
+    type: Number,
+  },
+  inter: {
+    type: String,
+  },
+  interPercent: {
+    type: Number,
+  },
+  graduation: {
+    type: String,
+  },
+  graduationStream: {
+    Type: String,
+  },
+  graduationCgpa: {
+    type: Number,
+  },
+  pg: {
+    type: String,
+  },
+  pgStream: {
+    type: String,
+  },
+  pgCgpa: {
+    type: String,
+  },
+  company: {
+    type: String,
+  },
+  duration: {
+    from: Date,
+    to: Date,
+  },
+  designation: {
+    type: String,
+    enum: [
+      "Software Developer",
+      "Forntend Developer",
+      "Full Stack Developer",
+      "UI/UX Designer",
+    ],
+  },
 });
-
 
 employeeSchema.index({ name: "text" });
 const EmployeeModel = mongoose.model("employee", employeeSchema);
-module.exports =
-  EmployeeModel
+module.exports = EmployeeModel;
