b12982209345e06fcae781a2341a931d5fa27723
sampleprince <princekumar887270@gmail.com>
2023-04-19 12:52:14 +0530

    null show

--- app/models/attendance.js
+++ app/models/attendance.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose');
 
 const attendanceSchema = new mongoose.Schema({
-    empId: { type: Number, required: true },
+    empId: { type: String, required: true },
     name: { type: String, required: true },
+    punch_in: { type: Date },
+    punch_out: { type: Date },
+    status: { type: String, required: true },
     date: { type: Date, required: true },
-    punch_in: { type: String, required: false },
-    punch_out: { type: String, required: false },
-    status: { type: String, required: true, enum: ["present", "absent", "leave"] },
 });
 
-module.exports = mongoose.model('Attendance', attendanceSchema);
+attendanceSchema.pre('save', function(next) {
+    if (this.isModified('status')) {
+        if (this.status === 'absent' || this.status === 'leave') {
+            this.punch_in = null;
+            this.punch_out = null;
+        } else if (this.status === 'present') {
+            if (!this.punch_in) {
+                this.punch_in = new Date();
+            }
+            if (this.isModified('punch_out') && this.punch_out) {
+                this.punch_out = new Date(this.punch_out);
+            }
+        }
+    }
+    next();
+});
+
+const Attendance = mongoose.model('Attendance', attendanceSchema);
+
+module.exports = Attendance;
