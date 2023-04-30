c6d1402a6e1a5b44ca565cb1fa475ba0338d5ee7
Adarsh3-90 <adarsh.kumar@venturepact.com>
2023-04-30 17:50:22 +0530

    /

--- app/routes/empRoute.js
+++ app/routes/empRoute.js
@@ -28,15 +28,15 @@ const upload=multer({storage:storage});
 
 
 router.post("/create", emplcontroller.createEmp);
-// router.post("/import",upload.single("csv"),emplcontroller.importUsers);
+router.post("/import",upload.single("csv"),emplcontroller.importUsers);
 router.get("/getEmployees",emplcontroller.getEmployees);
 router.get("/find", emplcontroller.getEmp);
 router.get("/uid", emplcontroller.generateUid);
-// router.post("/export",emplcontroller.exportUsers);
+router.post("/export",emplcontroller.exportUsers);
 router.get("/:id", emplcontroller.getsEmp);
 router.patch("/update/:id", emplcontroller.update);
 router.delete("/:id", emplcontroller.deleteEmployee);
-router.get("/detail/fetch",emplcontroller.employeedetail);
+router.get("/detail/fetch",emplcontroller.getEmployees);
 
 
 module.exports = router;
